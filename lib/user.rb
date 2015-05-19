062695470c71365537b65198da595a2da230cecd
Franz <franzknupfer@hotmail.com>
2015-05-19 11:00:22 -0700

    add initial validations and callbacks to User class; methods haven't been tested yet

--- lib/user.rb
+++ lib/user.rb
@@ -1,11 +1,22 @@
 class User <ActiveRecord::Base
 
-validates(:name, :presence => true)
+validates(:first_name, :presence => true)
+
+validates(:last_name, :presence => true)
 
 validates(:email, :presence => true)
 
 validates(:description, :presence => true)
+validates :description, length: { maximum: 500 }
 
 validates(:programmer_rating, :presence => true)
 
+before_validation(:titleize_name) do
+
+  private
+
+  define_method(:titleize_name) do
+    self.first_name = (first_name().titleize())
+    self.last_name = (last_name().titleize())
+  end
 end
